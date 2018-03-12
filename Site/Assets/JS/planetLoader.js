78e6ebe88e7e80058fe86b871507c5d6f778515f
Teito <Lasselaflair@gmail.com>
2018-03-12 11:17:07 +0100

    Testing JS
    Removed Commenting in planetLoader.JS

--- Site/Assets/JS/planetLoader.js
+++ Site/Assets/JS/planetLoader.js
@@ -1,24 +1,26 @@
 var rootURL = "https://swapi.co/api/";
 console.log("planetLoader script loaded")
 document.addEventListener('DOMContentLoaded', fetchPlanets);
-/*
+
 function fetchPlanets(){
     console.log("fetching");
     var load = true;
+
     while(load){
         var pageToLoad = 7;
         fetch(rootURL+'planets/?page='+pageToLoad).then(function(response){
-            return response.json();
+            var planets = JSON.parse(Response.responseText);
             console.log('Loading page: '+pageToLoad);
         }).then(function(planets){
             console.log(planets);
             console.log(response.next);
-            if(response.next != null){
-                console.log('Loading next page');
-                pageToLoad++;
-            }else{
+            if(response.next == null){
                 load = false;
             }
+            console.log('Loading next page');
+            pageToLoad++;
+            
         })
+        load = false;
     }
-}*/
+}
