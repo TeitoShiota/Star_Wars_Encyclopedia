cc150da4dba7964030474393fcfece80193dd32a
Shiota Teito <Lasselaflair@gmail.com>
2018-03-13 14:02:06 +0100

    changes to javascript

--- Site/Assets/JS/planetLoader.js
+++ Site/Assets/JS/planetLoader.js
@@ -13,9 +13,12 @@ function fetchPlanets(){
             for(k=0; k < planets.results.length; k++){
                 (function(y){
                 $(document.getElementById("innerContent")).append("<div class='divObject' id='item"+v+"'></div>");
-                $(document.getElementById("item"+v)).append("<h3 id='itemTitle'>"+""+planets.results[y].name+"</h3>");
-                $(document.getElementById("item"+v)).append("<p id='itemDiameter'>"+"Diameter: "+planets.results[y].diameter+"km"+"</p>");
-                $(document.getElementById("item"+v)).append("<p id='itemPopulation'>"+"Population: "+planets.results[y].population+"</p>");
+                $(document.getElementById("item"+v)).append("<h3 class='itemTitle'>"+""+planets.results[y].name+"</h3>");
+                $(document.getElementById("item"+v)).append("<p class='itemPopulation'>"+"Population: "+planets.results[y].population+"</p>");
+                $(document.getElementById("item"+v)).append("<p class='itemTerrain'>"+"Terrain: "+planets.results[y].terrain+"</p>");
+                $(document.getElementById("item"+v)).append("<p class='itemDiameter'>"+"Diameter: "+planets.results[y].diameter+"km"+"</p>");
+                $(document.getElementById("item"+v)).append("<p class='itemGravity'>"+"Gravity: "+planets.results[y].gravity+"</p>");
+                $(document.getElementById("item"+v)).append("<p class='itemPopulation'>"+"Population: "+planets.results[y].population+"</p>");
                 console.log(planets.results[y].name);
                 v++;
             })(k);
